b1376f20986a91a13f35078b1b31540a0b42ffa5
Alon-mizrahi <alonmiz1@gmail.com>
2021-04-17 14:39:18 +0200

    added iconography in cards and HUD's cards only display values if nor == 0, cards have + and - on them

---
+++ Micro Project 2 touched up/Assets/scripts/CardUnit.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardUnit : MonoBehaviour
+{
+    public string CardName;
+    public string CardDiscription;
+
+    //actual text elements
+    public Text cardType;
+
+//Player values and text
+    public float PlayerHPVal;
+    public float PlayerAtkModVal;
+    public float PlayerDefModVal;
+
+    public Text PlayerHPValtxt;
+    public Text PlayerAtkModValtxt;
+    public Text PlayerDefModValtxt;
+
+
+//Enemy values and text
+    public float EnemyHPVal;
+    public float EnemyAtkModVal;
+    public float EnemyDefModVal;
+
+    public Text EnemyHPValtxt;
+    public Text EnemyAtkModValtxt;
+    public Text EnemyDefModValtxt;
+
+
+    CardSystem cardsystem;
+
+    battleSystem battleSystem;
+    public GameObject battlesystm;
+    private void Start()
+    {
+        cardsystem = GameObject.Find("CardSystem").GetComponent <CardSystem>();
+        cardsystem.CardDescription.gameObject.SetActive(false);
+
+        battleSystem = battlesystm.GetComponent<battleSystem>();
+
+        cardType.text = CardName;
+
+
+
+        if (PlayerHPVal == 0)
+        {
+            PlayerHPValtxt.enabled = false;
+            //turn icon off? will have to make public image and redo cards
+        }
+        else {
+            if (PlayerHPVal > 0)
+            {
+                PlayerHPValtxt.text = "+" + PlayerHPVal;
+            }
+            else
+            {
+                PlayerHPValtxt.text = "" + PlayerHPVal;
+            }
+        }
+
+        if (PlayerAtkModVal == 0)
+        {
+            PlayerAtkModValtxt.enabled = false;
+            //turn icon off? will have to make public image and redo cards
+        }
+        else
+        {
+            if (PlayerAtkModVal > 0)
+            {
+                PlayerAtkModValtxt.text = "+" + PlayerAtkModVal;
+            }
+            else
+            {
+                PlayerAtkModValtxt.text = "" + PlayerAtkModVal;
+            }
+        }
+
+        if (PlayerDefModVal == 0)
+        {
+            PlayerDefModValtxt.enabled = false;
+            //turn icon off? will have to make public image and redo cards
+        }
+        else
+        {
+            if (PlayerDefModVal > 0)
+            {
+                PlayerDefModValtxt.text = "+" + PlayerDefModVal;
+            }
+            else
+            {
+                PlayerDefModValtxt.text = "" + PlayerDefModVal;
+            }
+        }
+
+        if (EnemyHPVal == 0)
+        {
+            EnemyHPValtxt.enabled = false;
+            //turn icon off? will have to make public image and redo cards
+        }
+        else
+        {
+            if (EnemyHPVal > 0)
+            {
+                EnemyHPValtxt.text = "+" + EnemyHPVal;
+            }
+            else
+            {
+                EnemyHPValtxt.text = "" + EnemyHPVal;
+            }
+        }
+
+        if (EnemyAtkModVal == 0)
+        {
+            EnemyAtkModValtxt.enabled = false;
+            //turn icon off? will have to make public image and redo cards
+        }
+        else
+        {
+            if (EnemyAtkModVal > 0)
+            {
+                EnemyAtkModValtxt.text = "+" + EnemyAtkModVal;
+            }
+            else
+            {
+                EnemyAtkModValtxt.text = "" + EnemyAtkModVal;
+            }
+        }
+
+        if (EnemyDefModVal == 0)
+        {
+            EnemyDefModValtxt.enabled = false;
+            //turn icon off? will have to make public image and redo cards
+        }
+        else
+        {
+            if (EnemyDefModVal > 0)
+            {
+                EnemyDefModValtxt.text = "+" + EnemyDefModVal;
+            }
+            else
+            {
+                EnemyDefModValtxt.text = "" + EnemyDefModVal;
+            }
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        // Widen the object by 0.1
+        transform.localScale = new Vector3(1.5f, 1.5f, 0.02f);
+        cardsystem.CardDescription.gameObject.SetActive(true);
+        if (CardName == "Brawler") { cardsystem.CardDescription.text = "Does high damage but its recklessness also hurts you"; }
+        else if (CardName == "Caster") { cardsystem.CardDescription.text = "Effects the enemys modifier values"; }
+        else if (CardName == "Healer") { cardsystem.CardDescription.text = "This card Focuses on healing and defence"; }
+        else if (CardName == "Soldier") { cardsystem.CardDescription.text = "Soldiers do low damage but increase your modifiers"; }//Soldiers are strong in groups. While holding in your hand it increases your Atk or Def modifiers
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale = new Vector3(1f, 1f, 0.02f);
+        cardsystem.CardDescription.gameObject.SetActive(false);
+    }
+
+
+    //Player card used CALLED BY CARD BUTTON PRESS
+    public void ATKCardUsed()
+    {
+        cardsystem.CardDescription.gameObject.SetActive(false);
+        // send card unit data to Attack card function (battlesystem)
+        battleSystem.OnAttackCard(PlayerHPVal, PlayerDefModVal, PlayerAtkModVal, this.gameObject, EnemyHPVal, EnemyDefModVal, EnemyAtkModVal);    
+    }
+
+
+    public void EnemyCardUsed()
+    {
+        battleSystem.EnemyCardUsed(PlayerHPVal, PlayerDefModVal, PlayerAtkModVal, this.gameObject, EnemyHPVal, EnemyDefModVal, EnemyAtkModVal);
+    }
+
+}
---
+++ Micro Project 2 touched up/Assets/scripts/EnemyAI.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyAI : MonoBehaviour
+{
+    CardSystem cardsystem;
+    string sceneName;
+    public unit enemyUnit;
+    public battleSystem battleScript;
+
+    string CardType;
+
+    private void Start()
+    {
+        cardsystem = GameObject.FindWithTag("CardSystem").GetComponent<CardSystem>();
+        battleScript = GameObject.Find("BattleSystem").GetComponent<battleSystem>();
+        //GameObject EnemyGO = GameObject.Find("Enemy");
+        //enemyUnit = EnemyGO.GetComponent<unit>();
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+    }
+
+//function called from battlestate
+    public void EnemyLevelCheck()
+    {
+//AI LEVEL 1 DUMB - PICK UP UNTIL FULL HAND THEN PLAY RANDOM CARD-------------------------------------------------------------------------------
+        if (sceneName == "1_level")
+        {
+            if (cardsystem.isTrueEnemyCardHolder1 == false || cardsystem.isTrueEnemyCardHolder2 == false || cardsystem.isTrueEnemyCardHolder3 == false || cardsystem.isTrueEnemyCardHolder4 == false || cardsystem.isTrueEnemyCardHolder5 == false) { cardsystem.OnDrawCard(); }
+            else
+            {
+                int x = Random.Range(1, 6);
+
+                //going to do random number gen and choose of the three cards
+                if (x == 1)
+                {
+                    cardsystem.EnemyCardBack1.SetActive(false);
+                    cardsystem.EnemyCardHolder1.transform.GetChild(0).GetComponent<CardUnit>().EnemyCardUsed();
+                }
+                else if (x == 2)
+                {
+                    cardsystem.EnemyCardBack2.SetActive(false);
+                    cardsystem.EnemyCardHolder2.transform.GetChild(0).GetComponent<CardUnit>().EnemyCardUsed();
+                }
+                else if (x == 3)
+                {
+                    cardsystem.EnemyCardBack3.SetActive(false);
+                    cardsystem.EnemyCardHolder3.transform.GetChild(0).GetComponent<CardUnit>().EnemyCardUsed();
+                }
+                else if (x == 4)
+                {
+                    cardsystem.EnemyCardBack4.SetActive(false);
+                    cardsystem.EnemyCardHolder4.transform.GetChild(0).GetComponent<CardUnit>().EnemyCardUsed();
+                }
+                else if (x == 5)
+                {
+                    cardsystem.EnemyCardBack5.SetActive(false);
+                    cardsystem.EnemyCardHolder5.transform.GetChild(0).GetComponent<CardUnit>().EnemyCardUsed();
+                }
+            }
+        }
+
+//AI LEVEL 2 DEFENSIVE - PICK UP UNTIL FULL THEN PLAY HEALING CARDS UNLESS HEALTH == MAX HEALTH---------------------------------------------------------
+        if (sceneName == "2_level")
+        {
+            if (cardsystem.isTrueEnemyCardHolder1 == false || cardsystem.isTrueEnemyCardHolder2 == false || cardsystem.isTrueEnemyCardHolder3 == false || cardsystem.isTrueEnemyCardHolder4 == false || cardsystem.isTrueEnemyCardHolder5 == false) { cardsystem.OnDrawCard(); }
+            else
+            {
+                //hand is full. now sort through and choose from priorety list
+
+                //enemy HP != max-> play healer else-> play atk
+                    for(int i=0; i< 4; i++)
+                    {
+                        //set order and cardType to look for
+                        if(i==0){ CardType = "Healer"; }
+                        else if(i==1){ CardType = "Caster"; }
+                        else if(i==2){ CardType = "Soldier"; }
+                        else if(i==3){ CardType = "Brawler"; }
+
+                        //Loop though cardplaces and look for the desired card type in order
+                        if (cardsystem.EnemyCardHolder1.transform.GetChild(0).GetComponent<CardUnit>().CardName == CardType)
+                        {
+                            cardsystem.EnemyCardBack1.SetActive(false);
+                            cardsystem.EnemyCardHolder1.transform.GetChild(0).GetComponent<CardUnit>().EnemyCardUsed();
+                            break;
+                        }
+                        else if (cardsystem.EnemyCardHolder2.transform.GetChild(0).GetComponent<CardUnit>().CardName == CardType)
+                        {
+                            cardsystem.EnemyCardBack2.SetActive(false);
+                            cardsystem.EnemyCardHolder2.transform.GetChild(0).GetComponent<CardUnit>().EnemyCardUsed();
+                            break;
+                        }
+                        else if (cardsystem.EnemyCardHolder3.transform.GetChild(0).GetComponent<CardUnit>().CardName == CardType)
+                        {
+                            cardsystem.EnemyCardBack3.SetActive(false);
+                            cardsystem.EnemyCardHolder3.transform.GetChild(0).GetComponent<CardUnit>().EnemyCardUsed();
+                            break;
+                        }
+                        else if (cardsystem.EnemyCardHolder4.transform.GetChild(0).GetComponent<CardUnit>().CardName == CardType)
+                        {
+                            cardsystem.EnemyCardBack4.SetActive(false);
+                            cardsystem.EnemyCardHolder4.transform.GetChild(0).GetComponent<CardUnit>().EnemyCardUsed();
+                            break;
+                        }
+                        else if (cardsystem.EnemyCardHolder5.transform.GetChild(0).GetComponent<CardUnit>().CardName == CardType)
+                        {
+                            cardsystem.EnemyCardBack5.SetActive(false);
+                            cardsystem.EnemyCardHolder5.transform.GetChild(0).GetComponent<CardUnit>().EnemyCardUsed();
+                            break;
+                        }
+
+                    }
+            }
+        }
+
+        //AI LEVEL 3 AGGRESIVE - PICK UP THREE PLAY BRAWLER FIRST THEN SODIER.
+        if (sceneName == "3_level")
+        {
+            if (cardsystem.isTrueEnemyCardHolder1 == false || cardsystem.isTrueEnemyCardHolder2 == false || cardsystem.isTrueEnemyCardHolder3 == false) { cardsystem.OnDrawCard(); }
+            else
+            {
+                //hand is full. now sort through and choose from priorety list
+
+                //enemy HP != max-> play healer else-> play atk
+                for (int i = 0; i < 4; i++)
+                {
+                    //set order and cardType to look for
+                    if (i == 0) { CardType = "Brawler"; } 
+                    else if (i == 1) { CardType = "Soldier"; }
+                    else if (i == 2) { CardType = "Caster"; }
+                    else if (i == 3) { CardType = "Healer"; }
+
+                    //Loop though cardplaces and look for the desired card type in order
+                    if (cardsystem.EnemyCardHolder1.transform.GetChild(0).GetComponent<CardUnit>().CardName == CardType)
+                    {
+                        cardsystem.EnemyCardBack1.SetActive(false);
+                        cardsystem.EnemyCardHolder1.transform.GetChild(0).GetComponent<CardUnit>().EnemyCardUsed();
+                        break;
+                    }
+                    else if (cardsystem.EnemyCardHolder2.transform.GetChild(0).GetComponent<CardUnit>().CardName == CardType)
+                    {
+                        cardsystem.EnemyCardBack2.SetActive(false);
+                        cardsystem.EnemyCardHolder2.transform.GetChild(0).GetComponent<CardUnit>().EnemyCardUsed();
+                        break;
+                    }
+                    else if (cardsystem.EnemyCardHolder3.transform.GetChild(0).GetComponent<CardUnit>().CardName == CardType)
+                    {
+                        cardsystem.EnemyCardBack3.SetActive(false);
+                        cardsystem.EnemyCardHolder3.transform.GetChild(0).GetComponent<CardUnit>().EnemyCardUsed();
+                        break;
+                    }
+                    /*else if (cardsystem.EnemyCardHolder4.transform.GetChild(0).GetComponent<CardUnit>().CardName == CardType)
+                    {
+                        cardsystem.EnemyCardBack4.SetActive(false);
+                        cardsystem.EnemyCardHolder4.transform.GetChild(0).GetComponent<CardUnit>().EnemyCardUsed();
+                        break;
+                    }
+                    else if (cardsystem.EnemyCardHolder5.transform.GetChild(0).GetComponent<CardUnit>().CardName == CardType)
+                    {
+                        cardsystem.EnemyCardBack5.SetActive(false);
+                        cardsystem.EnemyCardHolder5.transform.GetChild(0).GetComponent<CardUnit>().EnemyCardUsed();
+                        break;
+                    }*/
+
+                }
+            }
+        }
+    }
+
+}
